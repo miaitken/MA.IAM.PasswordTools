98f2c6f2de0b93a45246dbcf2b174c64c61f8a2c
Michael <mikeateken@gmail.com>
2025-10-26 13:03:17 -0400

    Update MA.IAM.PasswordTools/Public/Generate-MAPassword.ps1

--- MA.IAM.PasswordTools/Public/Generate-MAPassword.ps1
+++ MA.IAM.PasswordTools/Public/Generate-MAPassword.ps1
@@ -1,6 +1,12 @@
 <#
 .SYNOPSIS
 Generates a random password with a specified length and number of special characters.
+    Take Length parameter to determine overall length
+    Take SpecChar parameter to define number of special characters
+        Number of alphanumerics is equal to Length minus SpecChar
+            All alphanumerics and special characters selected from Alphanumerics and SpecialCharacters privates classes using RandomNumberGenerator
+    Scramble order of generated characters using RandomNumberGenerator
+    Outputs generated string
 
 .PARAMETER Length
 Total length of the password.
@@ -12,14 +18,14 @@ Number of special characters to include.
 Generate-MAPassword -Length 12 -SpecChar 4
 #>
 
-function Generate-MAPassword {
+function Generate-Password {
     [CmdletBinding()]
     param (
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [ValidateRange(1, [int]::MaxValue)]
         [int]$Length,
 
-        [Parameter(Mandatory=$true)]
+        [Parameter(Mandatory = $true)]
         [ValidateRange(0, [int]::MaxValue)]
         [int]$SpecChar
     )
@@ -31,23 +37,27 @@ function Generate-MAPassword {
     Add-Type -TypeDefinition @"
 using System;
 using System.Text;
+using System.Security.Cryptography;
 using System.Linq;
+
 public class PasswordGenerator {
+    private static readonly char[] Alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789".ToCharArray();
+    private static readonly char[] SpecialCharacters = "!@#$%^*()_-+=?".ToCharArray();
+
     public static string Generate(int length, int nonAlphanumericChars) {
-        const string alphanumerics = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
-        const string nonAlphanumerics = "!@#$%^*()_-+=?";
-        Random random = new Random();
-        StringBuilder password = new StringBuilder();
-        for (int i = 0; i < length - nonAlphanumericChars; i++) {
-            password.Append(alphanumerics[random.Next(alphanumerics.Length)]);
+        if (nonAlphanumericChars > length) throw new ArgumentException("Too many special characters requested.");
+        var chars = new char[length];
+        int i = 0;
+        for (; i < length - nonAlphanumericChars; i++) {
+            chars[i] = Alphanumerics[RandomNumberGenerator.GetInt32(Alphanumerics.Length)];
         }
-        for (int i = 0; i < nonAlphanumericChars; i++) {
-            password.Append(nonAlphanumerics[random.Next(nonAlphanumerics.Length)]);
+        for (; i < length; i++) {
+            chars[i] = SpecialCharacters[RandomNumberGenerator.GetInt32(SpecialCharacters.Length)];
         }
-        return new string(password.ToString().OrderBy(c => random.Next()).ToArray());
+        return new string(chars.OrderBy(c => RandomNumberGenerator.GetInt32(int.MaxValue)).ToArray());
     }
 }
 "@
 
-    return [PasswordGenerator]::Generate($Length,$SpecChar)
+    return [PasswordGenerator]::Generate($Length, $SpecChar)
 }
